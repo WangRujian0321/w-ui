dd7dea44bd4a9727c2e716327e0f864924aa9ea4
wrj <846741270@qq.com>
2022-06-23 21:55:23 +0800

    框架完成

---
+++ components/lib/card/src/card.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="m-card">
+    <div class="m-card-img">
+      <img src="" alt="">
+    </div>
+    <div class="m-card-summary">
+      summary
+    </div>
+    <div class="footer">
+      footer
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "w-card",
+  props:{
+    width: {
+      // 宽度
+      type: Number,
+      default: 0,
+    },
+    src: {
+      // 图片地址
+      type: String,
+      default: '',
+    },
+    imgHeight: {
+      type: Number,
+      default: 0,
+    },
+    summary: {
+      type: String,
+      default: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ examples/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '../components/css/demo.scss'
+import Demo from '../components/lib/demo/index'
+import Card from "../components/lib/card/index";
+// Vue.component('name', Demo);
+Vue.use(Demo); // Demo.install -> Vue.component();
+Vue.use(Card)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
