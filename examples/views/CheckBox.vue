c06738b61ebf1fa6ae940ab89feb5ad14f81b7f7
wrj <846741270@qq.com>
2022-06-27 13:58:05 +0800

    修正了radio的相关版式

--- examples/views/CheckBox.vue
+++ examples/views/CheckBox.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <p>111</p>
     <w_checkbox_group v-model="hobby">
       <w_checkbox label="篮球">篮球</w_checkbox>
       <w_checkbox label="足球">足球</w_checkbox>
       <w_checkbox label="乒乓球">乒乓球</w_checkbox>
     </w_checkbox_group>
+    <p>{{hobby}}</p>
   </div>
 </template>
 
